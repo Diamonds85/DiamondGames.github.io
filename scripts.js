f22f38dc8bdef1c24b8084d9692d672d9a2f9c60
Diamonds85 <2028edenfieldj@cocke.k12.tn.us>
2024-12-11 11:53:27 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
+const fonts = [
+  "customTitleChanger1",
+  "customTitleChanger2",
+  "customTitleChanger3",
+  "customTitleChanger4",
+  "customTitleChanger5",
+  "customTitleChanger6",
+  "customTitleChanger7",
+  "customTitleChanger8"
+];
 
+let fontIndex = 0
+
+function updateFont() {
+    // Update the CSS variable
+    document.documentElement.style.setProperty('--dynamic-font', fonts[currentFontIndex]);
+    currentFontIndex = (currentFontIndex + 1) % fonts.length; // Cycle to the next font
+}
+
+setInterval(updateFont, 2000);
